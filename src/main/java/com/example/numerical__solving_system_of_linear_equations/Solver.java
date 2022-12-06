df3fcdc02e29e146516e461cfdecd51bf3ce6fe4
Sarah SmithAbdullah <tamanabdullah9@gmail.com>
2022-12-06 15:57:21 +0200

    Solver Class Protoype

---
+++ src/main/java/com/example/numerical__solving_system_of_linear_equations/Solver.java
@@ -0,0 +1,33 @@
+package com.example.numerical__solving_system_of_linear_equations;
+
+import java.util.ArrayList;
+
+public class Solver
+{
+    ArrayList <Double> Solution = new ArrayList<>();
+    public ArrayList GaussJordan (double[][] input, double AbsoluteRelativeError){
+        /*
+        */
+        return Solution;
+    }
+    public ArrayList Gauss (double[][] input, double AbsoluteRelativeError){
+        /*
+         */
+        return Solution;
+    }
+    public ArrayList LU (double[][] input, String requiredForm, double AbsoluteRelativeError){
+        /*
+         */
+        return Solution;
+    }
+    public ArrayList GaussSeidel (double[][] input, double[] initialGuess, double AbsoluteRelativeError, int noIterations){
+        /*
+         */
+        return Solution;
+    }
+    public ArrayList Jacobi (double[][] input, double[] initialGuess, double AbsoluteRelativeError, int noIterations){
+        /*
+         */
+        return Solution;
+    }
+}
